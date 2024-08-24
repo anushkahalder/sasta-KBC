8dbe251e4c10f21bbd54702299b58e797f54edf5
Anushka Halder <ahalder@vitamix.com>
2024-08-24 15:51:57 +0530

    deploy 2

--- src/working/workingSlice.js
+++ src/working/workingSlice.js
@@ -1,7 +1,8 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 export const fetchQues = createAsyncThunk('fetchQues', async () => {
-    const data = await fetch('../src/data.json');
+    const data = await fetch(`${process.env.PUBLIC_URL}/data.json`);
+    // const data = await fetch(`../src/data.json`);
     const res = await data.json()
     return res;
 })
